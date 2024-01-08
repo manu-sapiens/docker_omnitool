725f156258ed39dbad91060150baf44230fefb76
Manu <manu@mercenaries.ai>
2024-01-08 18:32:41 +0800

    run_docker.sh will try to install docker if it isn't installed. .gitignore now exclude files in omnitool.data

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,27 @@
+echo "-----------------"
+# check if docker is installed
+if ! [ -x "$(command -v docker)" ]; then
+    # install docker
+    echo "[WARNING] Installing docker. This will require root access."
+    sudo apt-get update
+    # sudo sudo apt-get upgrade
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+    sudo usermod -aG docker Pi
+    if ! [ -x "$(command -v docker)" ]; then
+        echo '[ERROR]: FAILED to install docker'
+    fi
+fi
+
+
 # run the raspeberry pi docker accessible on port 1688, an map the data to ./om>
 echo "-----------------"
+echo "updating to latest omnitool_pi container"
+docker pull manusapiens/omnitool_pi
+
 echo "Already running Docker containers:"
 docker ps
-echo "-----------------"
 containers=$(docker container ls --format='{{json .}}')
 for container in $containers
 do
@@ -27,6 +46,7 @@ else
    fi
 fi
 
+echo "-----------------"
 echo "Starting Omnitool docker"
 docker run -p 1688:4444 -i -v ./omnitool.data:/app/omnitool/packages/omni-server/data.local manusapiens/omnitool_pi
 
